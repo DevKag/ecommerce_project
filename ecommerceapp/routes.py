5d8b2a540e8a485ba8a9b4c87b377c9d735b3077
DevKag <dkag@decoe.com>
2022-06-28 10:59:29 +0530

    Password reset feature development

--- ecommerceapp/routes.py
+++ ecommerceapp/routes.py
@@ -1,11 +1,13 @@
+from os import abort
 from flask import render_template, url_for, make_response, redirect, request, flash
 from flask import current_app as app
 from datetime import datetime
-from ecommerceapp import db
-from ecommerceapp.forms import RegistrationForm, LoginForm
 from flask_login import current_user, login_user, logout_user, login_required
+from ecommerceapp import db
+from ecommerceapp.forms import RegistrationForm, LoginForm, EmailForm, PasswordForm, UpdateAccountForm
 from ecommerceapp.model import User
 from ecommerceapp.token import generate_confirmation_token, confirm_token
+from ecommerceapp.email import send_email
 
 @app.route('/home')
 @app.route('/')
@@ -18,15 +20,21 @@ def register():
         return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        print(form.dob.data)
         user = User(lastname=form.lastname.data,firstname=form.firstname.data, dob=form.dob.data, 
                     address=form.address.data, email=form.email.data, password=form.password.data, 
                     confirmed=False)
         db.session.add(user)
         db.session.commit()
+
         token = generate_confirmation_token(user.email)
-        flash('Your account has been created!', 'success')
-        return redirect(url_for('login'))
+
+        confirm_url = url_for('confirm_email', token=token, _external=True)
+        html = render_template('activate.html', confirm_url=confirm_url)
+        subject = "Please confirm your email"
+        send_email(user.email, subject, html)
+
+        flash('A confirmation email has been sent via email.', 'success')
+        return redirect(url_for("home"))
     return render_template('register.html', title='Register', form=form)
 
 
@@ -38,9 +46,13 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and form.password.data:
-            login_user(user, remember=form.remember.data)
-            flash('Login Successful!', 'success')
-            return redirect(url_for('home'))
+            if user.confirmed:
+                login_user(user, remember=form.remember.data)
+                flash('Login Successful!', 'success')
+                return redirect(url_for('home'))
+            else:
+                flash('Please confirmed your account by clicking link send on your mail', 'danger')
+                return render_template('login.html', title='Login', form=form) 
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
@@ -49,4 +61,89 @@ def login():
 def logout():
     logout_user()
     flash('Logout Successful', 'success')
-    return render_template('logout.html')
+    return redirect(url_for('login'))
+
+@app.route('/confirm/<token>')
+def confirm_email(token):
+    try:
+        email = confirm_token(token)
+    except:
+        flash('The confirmation link is invalid or has expired.', 'danger')
+    user = User.query.filter_by(email=email).first_or_404()
+    if user.confirmed:
+        flash('Account already confirmed. Please login.', 'success')
+    else:
+        user.confirmed = True
+        user.confirmed_on = datetime.now()
+        db.session.add(user)
+        db.session.commit()
+        flash('You have confirmed your account. Thanks!', 'success')
+    return redirect(url_for('home'))
+
+@app.route('/reset', methods=["GET", "POST"])
+def reset():
+    form = EmailForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first_or_404()
+
+        subject = "Password reset requested"
+        token = generate_confirmation_token(form.email.data)
+
+        recover_url = url_for(
+            'reset_with_token',
+            token=token,
+            _external=True)
+
+        html = render_template(
+            'recover.html',
+            recover_url=recover_url)
+
+        # Let's assume that send_email was defined in myapp/util.py
+        send_email(user.email, subject, html)
+
+        return redirect(url_for('home'))
+    return render_template('reset.html', form=form)
+
+
+@app.route('/reset/<token>', methods=["GET", "POST"])
+def reset_with_token(token):
+    try:
+        email = confirm_token(token)
+    except:
+        abort(404)
+
+    form = PasswordForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=email).first_or_404()
+
+        user.password = form.password.data
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('login'))
+
+    return render_template('reset_with_token.html', form=form, token=token)
+
+@app.route("/account", methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.lastname = form.lastname.data
+        current_user.firstname = form.firstname.data
+        current_user.email = form.email.data
+        current_user.dob = form.dob.data
+        current_user.gender =  form.gender.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.lastname.data = current_user.lastname
+        form.firstname.data = current_user.firstname
+        form.email.data = current_user.email
+        form.dob.data = current_user.dob
+        form.gender.data = current_user.gender
+    return render_template('account.html', title='Account',
+                           form=form)
